0ee28e3b791083e357211f0e875e8b5354ed34c4
marioosh <marioosh.net@gmail.com>
2013-04-16 15:01:50 +0200

    old way (no async); comments;

--- app/controllers/PromiseController.java
+++ app/controllers/PromiseController.java
@@ -7,30 +7,48 @@ import play.libs.F.Promise;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+/**
+ * jmeter and many user thread show the difference between old and new way ;) 
+ * @author marioosh
+ *
+ */
 public class PromiseController extends Controller {
 
+	/**
+	 * old way
+	 * @return
+	 */
+	public static Result noasync1() {
+		try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }		
+		return ok(24 + "");
+	}
+	
+	/**
+	 * new way
+	 * @return
+	 */
 	public static Result async1() {
 
 		return async(
-				Akka.future(new Callable<Integer>() {
-					/**
-					 * long running job
-					 */
-					public Integer call() {
-						try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }
-						return 24;
-					}
-				}).map(
-						/**
-						 * callback function
-						 */
-						new Function<Integer, Result>() {
-							@Override
-							public Result apply(Integer arg0) throws Exception {
-								return ok(arg0 + "");
-							}			
-						}
-					)
+			Akka.future(new Callable<Integer>() {
+				/**
+				 * long running job
+				 */
+				public Integer call() {
+					try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }
+					return 24;
+				}
+			}).map(
+				/**
+				 * callback function
+				 */
+				new Function<Integer, Result>() {
+					@Override
+					public Result apply(Integer arg0) throws Exception {
+						return ok(arg0 + "");
+					}			
+				}
+			)
 		);
 
 	}
