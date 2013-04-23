9833cfef927c977d1da7dde059ae4f568d23b56d
marioosh <marioosh.net@gmail.com>
2013-04-23 11:30:50 +0200

    form2: form helpers

--- app/controllers/FormController.java
+++ app/controllers/FormController.java
@@ -5,6 +5,7 @@ import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.form;
+import views.html.form2;
 
 public class FormController extends Controller {
 
@@ -21,6 +22,23 @@ public class FormController extends Controller {
     		Message message = f.get();
     		flash("message", "message added");
     		return index();
+    	}
+    }
+
+	public static Result index2() {
+		Form<Message> f = Form.form(Message.class);
+		return ok(form2.render(f));
+    }
+	
+    public static Result form2post() {
+    	Form<Message> f = Form.form(Message.class);
+    	f = f.bindFromRequest();
+    	if(f.hasErrors()) {
+    		return badRequest(f.error("").message());
+    	} else {
+    		Message message = f.get();
+    		flash("message", "message added");
+    		return index2();
     	}
     }
 
